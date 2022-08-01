0138b1ac99b5806a50bf976f992757d358e26bef
guidoenr <guidoenr4@gmail.com>
2022-08-01 10:32:27 -0300

    guido:debian | 01-Aug-10:32

--- vtools.go
+++ vtools.go
@@ -1,4 +1,4 @@
-// Package vtools created by guidoenr, is a set of tools to code faster. Specifically written for veritone-engines/*
+// Package vtools created by guidoenr, is a set of tools to code faster. Specifically written for veritone-engines /*
 package vtools
 
 import (
@@ -6,20 +6,20 @@ import (
 	"reflect"
 )
 
-var Reset = "\033[0m"
-var Red = "\033[31m"
-var Green = "\033[32m"
-var Yellow = "\033[33m"
-var Blue = "\033[34m"
-var Purple = "\033[35m"
-var Cyan = "\033[36m"
-var Gray = "\033[37m"
-var White = "\033[97m"
+var reset = "\033[0m"
+var red = "\033[31m"
+var green = "\033[32m"
+var yellow = "\033[33m"
+var blue = "\033[34m"
+var purple = "\033[35m"
+var cyan = "\033[36m"
+var gray = "\033[37m"
+var white = "\033[97m"
 
-// Print the input in a pretty way
+// Print the input in a pretty way, and prints every type of var
 func Print(name string, in interface{}) {
 	fmt.Println("-----------------------------")
-	fmt.Printf(Cyan+"%s: %v"+Reset+" \n", name, in)
-	fmt.Printf(Gray+"type: %v"+Reset+"\n", reflect.TypeOf(in))
+	fmt.Printf(cyan+"%s: %v"+reset+" \n", name, in)
+	fmt.Printf(gray+"type: %v"+reset+"\n", reflect.TypeOf(in))
 	fmt.Println("-----------------------------")
 }
